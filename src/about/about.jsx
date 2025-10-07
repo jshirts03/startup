0944d7075b6dd30629a9575eafaf289a9b1dc78c
Jonah Shirts <jshirts03@gmail.com>
2025-10-07 12:47:13 -0600

    CSS fixes

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './about.css'
 
 export function About() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>about displayed here</div>
+    <main>
+        <div id="card_holder">
+            <div className="card" id="roc_card">
+                <img src="images/roc2.jpg" className="card-img-top" alt="roc"/>
+                <div className="card-body">
+                    <h5 className="card-title">About the ROC</h5>
+                    <p className="card-text">The ROC (short for "roar of the cougars") is the nickname for the student section at BYU.
+                    Known for being loud and 100% sober, it is known to cause opposing teams to quake in their boots.
+                    At least 5 false starts and 20 missed free throws have been caused by this awesome fanbase. </p>
+                    <a href="https://byucougars.com/sports/roc" className="btn btn-primary">ROC Website</a>
+                </div>
+            </div>
+
+            <div className="card" id="dev_card">
+                <img src="images/jonah2.jpg" id="dev_photo" className="card-img-top" alt="roc"/>
+                <div className="card-body">
+                    <h5 className="card-title">About the developer</h5>
+                    <p className="card-text">Jonah Shirts is a sophomore at BYU studying computer science. He loves BYU. Go Cougs! :)</p>
+                    <a id="dev_button" href="https://byucougars.com/sports/roc" className="btn btn-primary">ROC Website</a>
+                </div>
+            </div>
+        </div>
+        <div id="message_holder">
+            <p>Due to the popularity of BYU sports, the ROC consistently hits max capacity, leaving students to watch the games at home with no connection to their fellow fans. The virtual ROC does not aim to replicate the in-person ROC experience(it is impossible), but it does try to connect those loyal, strong, and true BYU fans to share in the victories and (very rare) defeats of the cougars.</p>
+        </div>
+
+        <section id="quotes">
+            <h4>In case of a BYU loss</h4>
+            <h6>Read ONLY if BYU loses</h6>
+            <p>It's ok, you can't win 'em all. Maybe this motivational quote will help.</p>
+            <p>INSERT MOTIVSTIONAL QUOTE HERE</p>
+        </section>
     </main>
   );
 }
--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './chat.css'
 
 export function Chat() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>chat displayed here</div>
+    <main className="container-fluid">
+        <h1>ROC CHAT</h1>
+        <p>real time messages will appear here: hoping the screen will load up with 10 most recent messages sent.</p>
+        <div id="rocchat_spacing">
+            <div className="blank"></div>
+            <div id="rocchat">
+                <div id="messages">
+                    <div className="message"><div className= "Username">Name</div><p id="message 10">chatline10</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 9">chatline9</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 8">chatline8</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 7">chatline7</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 6">chatline6</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 5">chatline5</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 4">chatline4</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 3">chatline3</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 2">chatline2</p></div>
+                    <div className="message"><div className= "Username">Name</div><p id="message 1">chatline1</p></div>
+                </div>
+                <form id="chat">
+                    <div id="chatbar">
+                        <input type="text" name="message" placeholder="go cougs!!" />
+                    </div>
+                    <div id="button">
+                        <button type="submit">Send</button>
+                    </div>
+                </form>
+            </div>
+            <div className="blank"></div>
+        </div>
+        <div>
+            <p>Messages sent: number (This will be recalled from memory and added to every time a message is sent)</p>
+        </div>
     </main>
   );
 }
