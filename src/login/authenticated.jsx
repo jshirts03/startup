32f9037b10c219540e4d04a27740e7b41774ed6b
Jonah Shirts <jshirts03@gmail.com>
2025-10-21 21:28:02 -0600

    Deletes the sent counter from local storage when you log out

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -7,6 +7,7 @@ export function Authenticated({userName, onLogout}){
 
     function logout() {
         localStorage.removeItem('userName');
+        localStorage.removeItem('sent')
         onLogout();
     }
 
