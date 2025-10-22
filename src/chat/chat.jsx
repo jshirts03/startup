04375062c8a5c1501d17ff6b01aae3f19e125188
Jonah Shirts <jshirts03@gmail.com>
2025-10-21 20:06:01 -0600

    Add the sendMessage function that updates the array and will display up to 10 messages

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -3,7 +3,17 @@ import './chat.css'
 
 export function Chat({userName}) {
     let [chats, setChats] = React.useState(localStorage.getItem('chat') || []);
+    let [message, setMessage] = React.useState();
 
+    React.useEffect(() => {console.log("Updated chats:", chats);}, [chats]);
+    
+    function sendMessage(message){
+        let chat = {name: userName, message: message}
+        if (chats.length === 10){
+            chats.pop()
+        }
+        setChats((chats) => [chat, ...chats])
+    }
 
   return (
     <main className="container-fluid">
@@ -14,42 +24,42 @@ export function Chat({userName}) {
             <div id="rocchat">
                 <div id="messages">
                     {chats.length >= 10 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 10">{chats[9].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 10">{chats[9].message}</p></div>
                     )}
                     {chats.length >= 9 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 9">{chats[8].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 9">{chats[8].message}</p></div>
                     )}
                     {chats.length >= 8 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 8">{chats[7].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 8">{chats[7].message}</p></div>
                     )}
                     {chats.length >= 7 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 7">{chats[6].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 7">{chats[6].message}</p></div>
                     )}
                     {chats.length >= 6 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 6">{chats[5].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 6">{chats[5].message}</p></div>
                     )}
                     {chats.length >= 5 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 5">{chats[4].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 5">{chats[4].message}</p></div>
                     )}
                     {chats.length >= 4 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 4">{chats[3].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 4">{chats[3].message}</p></div>
                     )}
                     {chats.length >= 3 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 3">{chats[2].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 3">{chats[2].message}</p></div>
                     )}
                     {chats.length >= 2 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 2">{chats[1].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 2">{chats[1].message}</p></div>
                     )}
                     {chats.length >= 1 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 1">{chats[0].chat}</p></div>
+                        <div className="message"><div className= "Username">Name</div><p id="message 1">{chats[0].message}</p></div>
                     )}
                 </div>
                 <form id="chat">
                     <div id="chatbar">
-                        <input type="text" name="message" placeholder="go cougs!!" />
+                        <input type="text" name="message" placeholder="go cougs!!" onChange={(e) => setMessage(e.target.value)}/>
                     </div>
                     <div id="button">
-                        <button type="submit">Send</button>
+                        <button type="submit" onClick={(e) => {e.preventDefault(); sendMessage(message);}}>Send</button>
                     </div>
                 </form>
             </div>
