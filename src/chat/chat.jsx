e5ad9dc187d52d6630ac7541cb96caf9b55f6abb
Jonah Shirts <jshirts03@gmail.com>
2025-10-21 20:23:03 -0600

    Chat messages cna be sent!!!

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './chat.css'
 
-export function Chat({userName}) {
-    let [chats, setChats] = React.useState(localStorage.getItem('chat') || []);
+export function Chat() {
+    let [chats, setChats] = React.useState(JSON.parse(localStorage.getItem("chat")) || []);
     let [message, setMessage] = React.useState();
 
     React.useEffect(() => {console.log("Updated chats:", chats);}, [chats]);
-    
+    React.useEffect(() => localStorage.setItem("chat", JSON.stringify(chats)), [chats]);
+
+
     function sendMessage(message){
+        let userName = localStorage.getItem("userName")
         let chat = {name: userName, message: message}
         if (chats.length === 10){
             chats.pop()
         }
         setChats((chats) => [chat, ...chats])
+        localStorage.setItem("chat", JSON.stringify(chats))
     }
 
   return (
@@ -24,34 +28,34 @@ export function Chat({userName}) {
             <div id="rocchat">
                 <div id="messages">
                     {chats.length >= 10 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 10">{chats[9].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[9].name}</div><p id="message 10">{chats[9].message}</p></div>
                     )}
                     {chats.length >= 9 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 9">{chats[8].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[8].name}</div><p id="message 9">{chats[8].message}</p></div>
                     )}
                     {chats.length >= 8 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 8">{chats[7].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[7].name}</div><p id="message 8">{chats[7].message}</p></div>
                     )}
                     {chats.length >= 7 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 7">{chats[6].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[6].name}</div><p id="message 7">{chats[6].message}</p></div>
                     )}
                     {chats.length >= 6 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 6">{chats[5].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[5].name}</div><p id="message 6">{chats[5].message}</p></div>
                     )}
                     {chats.length >= 5 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 5">{chats[4].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[4].name}</div><p id="message 5">{chats[4].message}</p></div>
                     )}
                     {chats.length >= 4 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 4">{chats[3].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[3].name}</div><p id="message 4">{chats[3].message}</p></div>
                     )}
                     {chats.length >= 3 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 3">{chats[2].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[2].name}</div><p id="message 3">{chats[2].message}</p></div>
                     )}
                     {chats.length >= 2 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 2">{chats[1].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[1].name}</div><p id="message 2">{chats[1].message}</p></div>
                     )}
                     {chats.length >= 1 &&(
-                        <div className="message"><div className= "Username">Name</div><p id="message 1">{chats[0].message}</p></div>
+                        <div className="message"><div className= "Username">{chats[0].name}</div><p id="message 1">{chats[0].message}</p></div>
                     )}
                 </div>
                 <form id="chat">
