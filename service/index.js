40d590fec3c8fa0b6ae3cd266015d58e88f98e44
Jonah Shirts <jshirts03@gmail.com>
2025-11-10 15:10:15 -0700

    Added middleware to send default routing

--- service/index.js
+++ service/index.js
@@ -16,6 +16,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.json())
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
 
 //This basically takes any requests that start with /api and send them to the apiRouter. That way we don't have to repeatedly type "/api"
 var apiRouter = express.Router();
