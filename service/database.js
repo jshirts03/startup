d5905def9e9d477a86e81074f1e165d230467a2d
Jonah Shirts <jshirts03@gmail.com>
2025-11-12 11:36:01 -0700

    Added database.js, updated the service to store chats and users in database instead of locally in the server

---
+++ service/database.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+// Connect to the database cluster
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const chatCollection = db.collection('chat');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addChat(chat) {
+  await chatCollection.insertOne(chat)
+}
+
+async function getChats() {
+  const options = {
+    sort: {_id : -1},
+    limit: 10
+  }
+  const chats = await chatCollection.find({},options)
+  const array = chats.toarray()
+  return array.reverse()
+}
+
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  addChat,
+  getChats,
+};
--- service/index.js
+++ service/index.js
@@ -3,11 +3,11 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
 //gonna store the users and the last 10 messages in the server now instead of local storage
-let users = [];
 let chats = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
@@ -28,7 +28,6 @@ apiRouter.post('/auth/create', async (req, res) => {
     }
     else{
         const user = await createUser(req.body.email, req.body.password);
-
         setAuthCookie(res, user.token);
         res.send({email: user.email});
     }
@@ -40,6 +39,7 @@ apiRouter.post('/auth/login', async (req,res) => {
     if (user){
         if (await bcrypt.compare(req.body.password, user.password)){
             user.token = uuid.v4();
+            await DB.updateUser(user);
             setAuthCookie(res, user.token);
             res.send({email: user.email});
             return;
@@ -53,6 +53,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
     delete user.token;
+    DB.updateUser(user)
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -70,6 +71,7 @@ const verifyAuth = async (req, res, next) => {
 }
 
 apiRouter.get('/chats', verifyAuth, async (req, res) => {
+    const chats = await DB.getChats()
     res.json(chats)
 })
 
@@ -82,6 +84,7 @@ apiRouter.get('/get/sent', verifyAuth, async (req, res) => {
 apiRouter.post('/send', verifyAuth, async (req, res) => {
     const user = findUser('token', req.cookies[authCookieName]);
     user.sent += 1
+    DB.updateUser(user)
     updateChats(req.body, res)
 })
 
@@ -90,11 +93,14 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-//checks our users array and returns the user that matches the field and value (if login or create is searches by email)
-function findUser(field, value){
-    if (!value) return null;
+//checks our users databse and returns the user that matches the field and value (if login or create is searches by email)
+async function findUser(field, value) {
+  if (!value) return null;
 
-    return users.find((u) => u[field] === value)
+  if (field === 'token') {
+    return DB.getUserByToken(value);
+  }
+  return DB.getUser(value);
 }
 
 
@@ -108,7 +114,7 @@ async function createUser(email, password){
         'token' : uuid.v4(),
         "sent" : 0
     };
-    users.push(user)
+    await DB.addUser(user)
     return user
 }
 
@@ -125,15 +131,8 @@ function setAuthCookie(res, token){
 
 async function updateChats(chat, res){
     if (chat) {
-        if (chats.length >= 10){
-            const trimmed = chats.slice(0, 9);
-            chats = [chat, ...trimmed];
-            res.status(200).send({msg:"Yay it worked"})
-        }
-        else{
-            chats = [chat, ...chats];
-            res.status(200).send({msg:"Yay it worked"})
-        }
+        await DB.addChat(chat)
+        res.status(200).send({msg: "Yayyyy"})
     }
     else{
         res.status(404).send({msg: "Nothing to send"});
