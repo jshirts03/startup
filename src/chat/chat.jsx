db6f92d43ad7a1a0a4a237120c469a9285f33f3c
Jonah Shirts <jshirts03@gmail.com>
2025-12-03 21:07:17 -0700

    establishing frontend websocket endpoints. Installing ws for Node

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -4,7 +4,8 @@ import './chat.css'
 export function Chat() {
     let [chats, setChats] = React.useState([]);
     let [message, setMessage] = React.useState();
-    let [sent, setSent] = React.useState(0)
+    let [sent, setSent] = React.useState(0);
+    let [status, setStatus] = React.useState("disconnected");
     
 // Initially gets the user chat count, and the 10 most recent chats
     React.useEffect(() => {
@@ -42,15 +43,20 @@ export function Chat() {
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
         socket.onopen = (event) => {
-         //Send a message into the chat titled "Username, joined"
+            let message = {name: userName, message: "JOINED"}
+            socket.send(JSON.stringify(message))
+            setStatus("connected")
         };
-        this.socket.onclose = (event) => {
+        socket.onclose = (event) => {
          //Send a message into the chat titled "Username, left"
+            setStatus("disconnected")
         };
-        this.socket.onmessage = async (msg) => {
+        socket.onmessage = async (msg) => {
         try {
             //turn message into readable elements
             //then set chats to include message as the first element of the array
+            message = JSON.parse(msg.data)
+            setChats((prevChats) => {const trimmed = prevChats.length >= 10 ? prevChats.slice(0, 9) : prevChats; return [chat, ...trimmed];})
         } catch {}
         };
 
@@ -69,6 +75,8 @@ export function Chat() {
             credentials: "include",
         });
         //instead of getting from the DB/server, I'll just add this to my local chats and send a websocket message to all the other groups in the same way, but update the DB
+        setChats((prevChats) => {const trimmed = prevChats.length >= 10 ? prevChats.slice(0, 9) : prevChats; return [chat, ...trimmed];})
+        socket.send(JSON.stringify(chat))
         await updateSent();
         //says that if the chats was greater than or equal to 10 mesages, it will slice it into only its first 10 elements (like trimmed = chats[:9])
         //setChats((prevChats) => {const trimmed = prevChats.length >= 10 ? prevChats.slice(0, 9) : prevChats; return [chat, ...trimmed];});
@@ -123,7 +131,9 @@ export function Chat() {
                     </div>
                 </form>
             </div>
-            <div className="blank"></div>
+            <div className="blank">
+                <p>{status}</p>
+            </div>
         </div>
         <div>
             <p>Messages sent: {sent} </p>
